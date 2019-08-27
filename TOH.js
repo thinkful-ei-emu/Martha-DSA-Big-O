3070c1bbdcba2319e0462f46ccb58a5329ffa6c2
Martha Plantz <maplantz89@gmail.com>
2019-08-27 14:03:09 -0700

    more on TOH

--- TOH.js
+++ TOH.js
@@ -11,9 +11,37 @@ function TowerOfHanoi (n, source, destination, temp){
 function print(source, destination){
   console.log(`${source} -> ${destination}`);
 }
-TowerOfHanoi(7, 'A', 'C', 'B');
 
-//console.log(TowerOfHanoi(2, 'A', 'C', 'B'));
+TowerOfHanoi(5, 'A', 'C', 'B');
 
 
+// If you are given 5 disks, how do the rods look like after 7 recursive calls?
+
+/**
+ * 1 (smallest) -> c 
+ * 2 -> b
+ * 1 ->b
+ * 3->c
+ * 1->a
+ * 2->c
+ * 1-> c
+ * 
+ * after 7 recursive calls
+ * Col 1       |  Col 2       |    Col 3 
+ *  4          |              |     1(smallest)
+ *  5(largest) |    2         |     3
+ */
+
+// How many moves are needed to complete the puzzle with 3 disks? with 4 disks? with 5 disks?
+/**
+ * 
+ * 3- 7 moves
+ * 4- 15 moves
+ * 5- 31 moves
+ *  
+*/
+
+// What is the runtime of your algorithm?
+//for 4 rings 9.385ms
+//for 5 rings 9.850ms
 
